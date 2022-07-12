a331471e99bf2b00dbd1bf9e4a8b1b0665a30386
Valentin Panov <v.i.panov@esoft.local>
2022-07-12 13:11:14 +0500

    first commit

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ hello }}
+    {{ data.gem }}
+    <v-btn @click="changeSmth">
+      Изменить что то
+    </v-btn>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  const hello = ref('Hello World Photoraviga');
+  const { data, refresh } = await useFetch('/api/gem');
+
+  const changeSmth = () => {
+    refresh();
+  }
+</script>
